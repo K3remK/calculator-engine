0b74a3f4c83ef640d9ecfd22d0d17040f20791a6
K3remK <kerem7812@gmail.com>
2026-02-28 15:03:46 +0300

    removed the local files and executable from repo

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kerem/Desktop/dev/calculator-backend
-# Build directory: /home/kerem/Desktop/dev/calculator-backend/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
