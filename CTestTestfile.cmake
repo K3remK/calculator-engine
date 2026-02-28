a8ba9e2f4d04d03bacc411b72614a1d473b5dfc3
K3remK <kerem7812@gmail.com>
2026-02-28 14:49:56 +0300

    -changed Parse logic to be more memory efficient and fast and more scalable for the future features

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kerem/Desktop/dev/calculator-backend
+# Build directory: /home/kerem/Desktop/dev/calculator-backend/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
